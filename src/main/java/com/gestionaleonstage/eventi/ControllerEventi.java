bb3de75a4cefaa53a20ad8bc2b9a7c769e45942a
Federico Calò <fedcal01@gmail.com>
2023-03-11 11:00:43 +0100

    [ Aggiungi Tipologia Evento ] - init views and controller

--- src/main/java/com/gestionaleonstage/eventi/ControllerEventi.java
+++ src/main/java/com/gestionaleonstage/eventi/ControllerEventi.java
@@ -1,15 +1,29 @@
 package com.gestionaleonstage.eventi;
 
+import com.gestionaleonstage.HomeApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ControllerEventi {
-    @FXML
-    private Button btnAggiungiEvento;
-    @FXML
+    private Button btnAvviaBloccaEvento;
+    private Button btnTipologiaEvento;
     private Button btnVisualizzaEventi;
-    @FXML
     private Button btnModificaEventi;
-    @FXML
-    private Button btnGestisciEvento;
+
+    public void viewTipologia() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HomeApplication.class.getResource("eventi/tipologiaEventi/homeGestioneTipologiaEventi.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.setTitle("Gestionale Associativo - Gestione Tipologia Eventi");
+        stage.show();
+    }
 }
---
+++ src/main/java/com/gestionaleonstage/eventi/ControllerTipologiaEventi.java
@@ -0,0 +1,13 @@
+package com.gestionaleonstage.eventi;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControllerTipologiaEventi {
+    @FXML
+    private Button aggiungiTipologia;
+    @FXML
+    private Button modificaTipologia;
+    @FXML
+    private Button infoTipologia;
+}
